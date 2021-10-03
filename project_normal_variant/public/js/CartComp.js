82a8e18d583325c5cdfa3cbf1cc18618a268753d
Julpa <you@example.comJulpart@yandex.ru>
2021-10-03 07:09:31 +0700

    lesson8

--- project_normal_variant/public/js/CartComp.js
+++ project_normal_variant/public/js/CartComp.js
@@ -8,40 +8,35 @@ Vue.component('cart', {
       }
     },
     methods: {
-        aaa(){
-            console.log("я жив");
-        },
+        
         addProduct(product){
-            this.$parent.getJson(`${API}/addToBasket.json`)
-                .then(data => {
-                    if(data.result === 1){
-                        let find = this.cartItems.find(el => el.id_product === product.id_product);
-                        if(find){
-                            find.quantity++;
-                        } else {
-                            let prod = Object.assign({quantity: 1}, product);
-                            this.cartItems.push(prod)
-                        }
-                    } else {
-                        alert('Error');
-                    }
-                })
+            let find = this.cartItems.find(el => el.id_product === product.id_product);
+            if(find){
+                this.$parent.putJson(`/api/cart/${find.id_product}/${1}`);
+                find.quantity++;
+            } else {
+                let prod = Object.assign({quantity: 1}, product);
+                this.$parent.postJson('/api/cart', prod)
+                  .then(data => {
+                      if (data.result === 1) {
+                          this.cartItems.push(prod);
+                      }
+                  });
+            }
         },
-        remove(item) {
-            this.$parent.getJson(`${API}/deleteFromBasket.json`)
-                .then(data => {
-                    if(data.result === 1) {
-                        if(item.quantity>1){
-                            item.quantity--;
-                        } else {
-                            this.cartItems.splice(this.cartItems.indexOf(item), 1)
-                        }
-                    }
-                })
+        remove(product) {
+            let find = this.cartItems.find(el => el.id_product === product.id_product);
+            if(find.quantity>1){
+                this.$parent.putJson(`/api/cart/${find.id_product}/${-1}`);
+                find.quantity--;
+            } else {
+                this.$parent.deleteJson(`/api/cart/${find.id_product}`)
+                this.cartItems.splice(this.cartItems.indexOf(find), 1)
+            }
         },
     },
     mounted(){
-        this.$parent.getJson(`${API + this.cartUrl}`)
+        this.$parent.getJson('/api/cart')
             .then(data => {
                 for(let el of data.contents){
                     this.cartItems.push(el);
--- project_normal_variant/public/js/SearchComp.js
+++ project_normal_variant/public/js/SearchComp.js
@@ -6,9 +6,9 @@ Vue.component('search',{
     },
 
     template: `
-    <form action="#" class="search-form" @submit.prevent="filter">
+    <form action="#" class="search-form" @submit.prevent="$parent.$refs.products.filter(userSearch)">
         <input type="text" class="search-field" v-model="userSearch">
-        <button class="btn-search" type="submit" @click="$parent.$refs.products.filter(userSearch)">
+        <button class="btn-search" type="submit" >
             <i class="fas fa-search"></i>
         </button>
     </form>`
